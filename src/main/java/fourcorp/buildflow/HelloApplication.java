58a8f5c0b0efebf4d0ff54e8cb7726c4921d2181
Ricardo Meireles <1230744@isep.ipp.pt>
2024-10-05 19:09:18 +0100

    Preloader JavaFX screen test.

--- src/main/java/fourcorp/buildflow/HelloApplication.java
+++ src/main/java/fourcorp/buildflow/HelloApplication.java
@@ -1,20 +1,33 @@
 package fourcorp.buildflow;
 
+import javafx.animation.PauseTransition;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("preloader/preloading-screen.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("BUILDFLOW");
         stage.setScene(scene);
+        stage.setAlwaysOnTop(true);
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.centerOnScreen();
         stage.show();
+
+        PauseTransition pause = new PauseTransition(Duration.seconds(10));
+        pause.setOnFinished(event -> {
+            Platform.exit();
+        });
+        pause.play();
     }
 
     public static void main(String[] args) {
