31d293de6076bd993bf5a9892315d0740005d86e
Joana Gama <1230399@isep.ipp.pt>
2024-10-15 17:36:56 +0100

    Added more tests for the simulator class  #31 #91 #92 #93 #94"

--- src/test/java/fourcorp/buildflow/application/SimulatorTest.java
+++ src/test/java/fourcorp/buildflow/application/SimulatorTest.java
@@ -1,8 +1,11 @@
 package fourcorp.buildflow.application;
 
 import fourcorp.buildflow.domain.Operation;
+import fourcorp.buildflow.domain.PriorityOrder;
 import fourcorp.buildflow.domain.Product;
 import fourcorp.buildflow.domain.Workstation;
+import fourcorp.buildflow.repository.ProductPriorityLine;
+import fourcorp.buildflow.repository.Repositories;
 import fourcorp.buildflow.repository.WorkstationsPerOperation;
 import org.junit.jupiter.api.Test;
 
@@ -44,7 +47,7 @@ class SimulatorTest {
         a.add(product10);
 
         Simulator s = new Simulator();
-        s.run(a);
+        s.runWithoutPriority(a);
 
         List<Operation> operations1 = s.getOperationQueues().getByKey(product1);
         List<Operation> operations2 = s.getOperationQueues().getByKey(product2);
@@ -71,7 +74,7 @@ class SimulatorTest {
     }
 
     @Test
-    void processItems() {
+    void processItemsWithoutPriority() {
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
 
@@ -82,7 +85,7 @@ class SimulatorTest {
 
         WorkstationsPerOperation w = new WorkstationsPerOperation();
 
-        Simulator simulator = new Simulator(w);
+        Simulator simulator = new Simulator(w, Repositories.getInstance().getProductPriorityRepository());
 
         Workstation ws1 = new Workstation("WS1", 10);
         Workstation ws2 = new Workstation("WS2", 8);
@@ -94,12 +97,87 @@ class SimulatorTest {
         w.create(ws3, new Operation("Welding"));
         w.create(ws4, new Operation("Painting"));
 
-        simulator.run(products);
+        simulator.runWithoutPriority(products);
 
         String output = outContent.toString();
 
         assertTrue(output.contains("Current operation: Cutting"), "The cutting operation should have been processed.");
         assertTrue(output.contains("The best machine: WS1"), "The WS1 machine should have been used for the cutting operation.");
+
+        System.setOut(System.out);
+    }
+    @Test
+    void processItemsWithPriority() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        WorkstationsPerOperation w = new WorkstationsPerOperation();
+        ProductPriorityLine a = new ProductPriorityLine();
+
+        Product product1 = new Product("P001", new LinkedList<>(Arrays.asList(new Operation("Cutting"), new Operation("Welding"))));
+        Product product2 = new Product("P002", new LinkedList<>(Arrays.asList(new Operation("Assembling"), new Operation("Painting"))));
+        Product product3 = new Product("P003", new LinkedList<>(Arrays.asList(new Operation("Drilling"), new Operation("Polishing"))));
+        Product product4 = new Product("P004", new LinkedList<>(Arrays.asList(new Operation("Grinding"), new Operation("Coating"))));
+        Product product5 = new Product("P005", new LinkedList<>(Arrays.asList(new Operation("Forging"), new Operation("Inspection"))));
+        Product product6 = new Product("P006", new LinkedList<>(Arrays.asList(new Operation("Casting"), new Operation("Packaging"))));
+        Product product7 = new Product("P007", new LinkedList<>(Arrays.asList(new Operation("Cutting"), new Operation("Bending"), new Operation("Welding"))));
+        Product product8 = new Product("P008", new LinkedList<>(Arrays.asList(new Operation("Assembling"), new Operation("Testing"), new Operation("Painting"))));
+
+        a.create(product1, PriorityOrder.LOW);
+        a.create(product2, PriorityOrder.NORMAL);
+        a.create(product3, PriorityOrder.HIGH);
+        a.create(product4, PriorityOrder.NORMAL);
+        a.create(product5, PriorityOrder.LOW);
+        a.create(product6, PriorityOrder.HIGH);
+        a.create(product7, PriorityOrder.NORMAL);
+        a.create(product8, PriorityOrder.LOW);
+
+        Simulator simulator = new Simulator(w, a);
+
+        Workstation ws1 = new Workstation("WS1", 10);
+        Workstation ws2 = new Workstation("WS2", 8);
+        Workstation ws3 = new Workstation("WS1", 10);
+        Workstation ws4 = new Workstation("WS2", 8);
+        Workstation ws5 = new Workstation("WS3", 12);
+        Workstation ws6 = new Workstation("WS4", 15);
+        Workstation ws7 = new Workstation("WS5", 9);
+        Workstation ws8 = new Workstation("WS6", 11);
+        Workstation ws9 = new Workstation("WS7", 13);
+        Workstation ws10 = new Workstation("WS8", 14);
+        Workstation ws11 = new Workstation("WS9", 16);
+        Workstation ws12 = new Workstation("WS10", 10);
+        Workstation ws13 = new Workstation("WS11", 8);
+        Workstation ws14 = new Workstation("WS12", 14);
+        Workstation ws15 = new Workstation("WS13", 10);
+
+
+        w.create(ws1, new Operation("Cutting"));
+        w.create(ws2, new Operation("Assembling"));
+        w.create(ws3, new Operation("Welding"));
+        w.create(ws4, new Operation("Painting"));
+        w.create(ws5, new Operation("Drilling"));
+        w.create(ws6, new Operation("Polishing"));
+        w.create(ws7, new Operation("Grinding"));
+        w.create(ws8, new Operation("Coating"));
+        w.create(ws9, new Operation("Forging"));
+        w.create(ws10, new Operation("Inspection"));
+        w.create(ws11, new Operation("Casting"));
+        w.create(ws12, new Operation("Packaging"));
+        w.create(ws13, new Operation("Bending"));
+        w.create(ws14, new Operation("Testing"));
+        w.create(ws15, new Operation("Painting"));
+
+        simulator.runWithPriority();
+
+        String output = outContent.toString();
+
+        assertTrue(output.contains("Processing product " + product3.getId() + " in machine " + ws5.getId() + " - Estimated time: " + ws5.getTime() + " min"), "The drilling operation should have been processed.");
+        assertTrue(output.contains("Processing product " + product6.getId() + " in machine " + ws11.getId() + " - Estimated time: " + ws11.getTime() + " min"), "The casting operation should have been processed.");
+        assertTrue(output.contains("Processing product " + product2.getId() + " in machine " + ws2.getId() + " - Estimated time: " + ws2.getTime() + " min"), "The assembling operation should have been processed.");
+        assertTrue(output.contains("Processing product " + product2.getId() + " in machine " + ws4.getId() + " - Estimated time: " + ws4.getTime() + " min"), "The painting operation should have been processed.");
+        assertTrue(output.contains("Processing product " + product8.getId() + " in machine " + ws2.getId() + " - Estimated time: " + ws2.getTime() + " min"), "The assembling operation should have been processed.");
+
+
 
         System.setOut(System.out);
     }
