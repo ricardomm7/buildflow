add5c0d1e3a3997965d37a4f32c8fd57696dd10c
User <1240587@isep.ipp.pt>
2024-10-21 09:58:35 +0100

    USEI06 #29, #82, #83, #84 corrigi alguns metodos que davam erro mudei a forma como guardava informação em relação ao tempo.

--- src/main/java/fourcorp/buildflow/domain/Workstation.java
+++ src/main/java/fourcorp/buildflow/domain/Workstation.java
@@ -4,92 +4,29 @@ import java.util.HashMap;
 
 public class Workstation implements Identifiable<String> {
     private String idMachine;
-    private double time;  // Tempo de execução da operação
+    private double time;
+    private long waitingTime;
     private boolean isAvailable;
     private long startWaiting;
     private long stopWaiting;
     private int waitingCounter;
     private int oprCounter;
+    private long totalWaiting;
+    private long totalOper;
 
-    private double operationTimeTotal;
-    private double executiontimeTotal; // Este tem de ser igual ao operationTimeTotal + waitingTimeTotal : FALTA IMPLEMENTAR POIS ESTA CLASSE TÁ COM REGRAS FALHADAS
-
-    HashMap<String, Long> timeMap = new HashMap<>();
-    HashMap<String, Long> waitingMap = new HashMap<>();
 
     public Workstation(String idMachine, double time) {
         this.idMachine = idMachine;
         this.time = time;
         this.isAvailable = true;
+        this.startWaiting = 0;
+        this.stopWaiting = 0;
+        this.waitingCounter = 0;
+        this.oprCounter = 0;
+        this.waitingTime = 0;
+        this.totalWaiting = 0;
+        this.totalOper = 0;
     }
-
-    public void processProduct(Product product) {
-        this.setAvailable(false);
-        System.out.println("Processing product " + product.getId() + " in machine " + idMachine + " - Estimated time: " + time + " min");
-        operationTimeTotal += time;
-        new Thread(() -> {
-            simulateExecutionTime();
-            this.setAvailable(true);
-        }).start();
-    }
-
-    private void simulateExecutionTime() {
-        try {
-            long sleepTime = (long) (time * 0.00015);
-            Thread.sleep(sleepTime);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
-    }
-
-    public void setTimePerOperation(String idWorkstation, long time) {
-        long tmp = timeMap.get(idWorkstation);
-        tmp = time + tmp;
-        timeMap.put(idWorkstation, tmp);
-    }
-
-    public void startWaiting() {
-        setStartWaiting();
-    }
-
-    public void stopWaiting(String idWorkstation, Workstation workstation) {
-        workstation.setStopWaiting();
-        workstation.setWaitingCounter();
-        long waiting = workstation.getStartWaiting() - workstation.getStopWaiting();
-        waitingMap.put(idWorkstation, waiting);
-    }
-
-    public long getTotalTimePerOperation(String idWorkstation) {
-        long time = timeMap.get(idWorkstation);
-        return time;
-    }
-
-    public long getTotalWaitingTimePerOperation(String idWorkstation) {
-        long time = waitingMap.get(idWorkstation);
-        return time;
-    }
-
-    public long getAverageTotalTimePerOperation(String idWorkstation) {
-        long time = timeMap.get(idWorkstation);
-        time = time / getOprCounter();
-        return time;
-    }
-
-    public long getAverageTotalWaitingTimePerOperation(String idWorkstation) {
-        long time = timeMap.get(idWorkstation);
-        time = time / getWaitingCounter();
-        return time;
-    }
-
-
-    public double getExecutiontimeTotal() {
-        return executiontimeTotal;
-    }
-
-    public double getOperationTimeTotal() {
-        return operationTimeTotal;
-    }
-
     public void setOprounter() {
         this.oprCounter = oprCounter + 1;
     }
@@ -97,7 +34,6 @@ public class Workstation implements Identifiable<String> {
     public int getOprCounter() {
         return oprCounter;
     }
-
     public void setWaitingCounter() {
         this.waitingCounter = waitingCounter + 1;
     }
@@ -106,7 +42,7 @@ public class Workstation implements Identifiable<String> {
         return waitingCounter;
     }
 
-    public long getStartWaiting() {
+    public long getStartWaiting(){
         return startWaiting;
     }
 
@@ -114,12 +50,20 @@ public class Workstation implements Identifiable<String> {
         this.startWaiting = System.currentTimeMillis();
     }
 
-    public long getStopWaiting() {
-        return stopWaiting;
+    public long getStopWaiting(){
+        return stopWaiting ;
     }
 
     public void setStopWaiting() {
         this.stopWaiting = System.currentTimeMillis();
+    }
+
+    public String getIdMachine() {
+        return idMachine;
+    }
+
+    public void setIdMachine(String idMachine) {
+        this.idMachine = idMachine;
     }
 
     public double getTime() {
@@ -134,8 +78,73 @@ public class Workstation implements Identifiable<String> {
         return isAvailable;
     }
 
-    public void setAvailable(boolean available) {
+    public synchronized void setAvailable(boolean available) {
         isAvailable = available;
+    }
+
+    public void processProduct(Product product) {
+
+        this.setAvailable(false);
+        setOprounter();
+        if(startWaiting != 0 ){
+            setStopWaiting();
+            stopWaiting();
+
+        }
+
+        System.out.println("Processing product " + product.getId() + " in machine " + idMachine + " - Estimated time: " + time + " min");
+
+        // Simular o processamento em uma nova thread
+        new Thread(() -> {
+            simulateExecutionTime();  // Simular o tempo de execução
+            this.setAvailable(true);
+            setStartWaiting();
+            totalOper = totalOper + (long)time;
+            // Máquina fica disponível novamente após o tempo
+            //System.out.println("Machine " + idMachine + " is now available again.");
+        }).start();
+    }
+
+    private void simulateExecutionTime() {
+        try {
+            // Calcula o tempo de sleep: 1 segundo = 1 minuto na simulação
+            // O fator 0.00015 é usado para reduzir o tempo para fins de demonstração
+            long sleepTime = (long) (time * 0.00015);
+
+            Thread.sleep(sleepTime);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            //System.out.println("Execução interrompida para a máquina " + idMachine);
+        }
+    }
+
+
+    // conta e guarda o temp de uma operação // ja não é preciso
+    public void setTimePerOperation (double time){
+    }
+
+
+    //da stop a espera e guarda o tempo de espera
+    public void stopWaiting (){
+        setWaitingCounter();
+        long waiting =   stopWaiting- startWaiting;
+        waitingTime = waiting;
+        totalWaiting = totalWaiting + waiting;
+
+    }
+    public double getTotalTimePerOperation(){
+        return totalOper;
+    }
+    public long getTotalWaitingTimePerOperation(){
+        return totalWaiting;
+    }
+    public double getAverageTotalTimePerOperation(){
+        long time = totalOper / oprCounter;
+        return time;
+    }
+    public long getAverageTotalWaitingTimePerOperation(){
+        long time = totalWaiting / waitingCounter;
+        return time;
     }
 
     @Override
