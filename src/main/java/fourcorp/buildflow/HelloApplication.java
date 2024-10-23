55a99b8d8d787f806a0402866f3f7791571f5bac
Joana Gama <1230399@isep.ipp.pt>
2024-10-23 16:17:51 +0100

    Created a file with the BOM of different products. Creates a method to read said file. #34 #107

--- src/main/java/fourcorp/buildflow/HelloApplication.java
+++ src/main/java/fourcorp/buildflow/HelloApplication.java
@@ -1,6 +1,7 @@
 package fourcorp.buildflow;
 
 import fourcorp.buildflow.application.Reader;
+import fourcorp.buildflow.application.ReaderCSV;
 import fourcorp.buildflow.application.ReaderToSQL;
 import fourcorp.buildflow.ui.Menu;
 
@@ -25,6 +26,7 @@ public class HelloApplication extends Application {
             //ReaderToSQL.readExcelAndGenerateSQL("textFiles/Dataset01_v1.xlsx", "outFiles/01_v1.sql");
             //ReaderToSQL.readExcelAndGenerateSQL("textFiles/Dataset01_v2.xlsx", "outFiles/01_v2.sql");
             ReaderToSQL.readExcelAndGenerateSQL("textFiles/Dataset02_v1.xlsx", "outFiles/02_v1.sql");
+            ReaderCSV.saveInformation("textFiles/BOM.csv");
         } catch (Exception e) {
             System.out.println("Error uploading files: " + e.getMessage());
             return;
