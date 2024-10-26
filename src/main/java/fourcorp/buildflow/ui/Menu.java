d80aa3b1cf8c240a0fdda0261a50bf92a2d10db2
Bruno Teixeira <1230741@isep.ipp.pt>
2024-10-26 18:56:42 +0100

    Rollback to simulator and menu

--- src/main/java/fourcorp/buildflow/ui/Menu.java
+++ src/main/java/fourcorp/buildflow/ui/Menu.java
@@ -112,10 +112,6 @@ public class Menu {
             case 10:
                 s.printAverageWaitingTimes();
                 break;
-
-            case 11:
-                s.printAverageTimePerOperation();
-                break;
             case 0:
                 System.out.println("Exiting...");
                 scanner.close();
