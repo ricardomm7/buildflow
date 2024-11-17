8417ce4efe85526a7a195e8dd0fed5046edb84dd
User <1240587@isep.ipp.pt>
2024-11-17 16:57:57 +0000

    USACO5- tá o buffer feito retorna 1 sempre que carrega no ultimo elemento ou seja qd está cheio

--- others/machManager/USAC05/asm.s
+++ others/machManager/USAC05/asm.s
@@ -1,57 +1,50 @@
 .section .text
 .global enqueue_value
 
+
+	# %rdi --> buffer (*int)
+    # %rsi --> length (int)
+    # %rdx --> tail (*int)
+    # %rcx --> head (*int)
+    # %r8  --> value (int)
+    
 enqueue_value:
-   
-	push %rbx           
-	push %rbp            
-	push %r12            
-	push %r13           
-	push %r14            
+   xorq %rax, %rax
+    
+    cmp (%rdx), %rsi
+    je restart_buffer
+    
 
-   
-	mov 0x0(%rdx), %r12  
-	mov 0x0(%rcx), %r13
-	mov %r8, %r14 
+    movq (%rdx), %rax
+    imulq $4, %rax
+    addq %rax, %rdi
 
-	lea (%rdi, %r12, 4), %rbx 
-	mov %r14, (%rbx)
+    movl %r8d, (%rdi)
+    
+    # Incrementa o índice
+    movq (%rdx), %rax
+    addq $1, %rax
+    movq %rax, (%rdx)
+    jmp end
 
-	add $1, %r12               
-	cmp %rsi, %r12             
-	je reset_head
-	jmp continue_head
-	               
-reset_head:
-	mov $0, %r12
-	
-continue_head:
-	mov %r12, 0x0(%rdx)
+restart_buffer:
+	movq $0, (%rdx)
+	jmp enqueue_value
 
-	cmp %r12, %r13            
-	je full_buffer               
+end:
 
-	mov $0, %rax               
-	jmp end_function
+    # Carrega o índice do último elemento com base no comprimento do buffer
+    movq %rsi, %rax       
+    sub $1, %rax             
+    imulq $4, %rax        
+    addq %rax, %rdi       
 
-full_buffer:
-	add $1, %r13             
-	cmp %rsi, %r13           
-	je reset_tail            
-	jmp continue_tail
-
-reset_tail:
-	mov $0, %r13             
-
-continue_tail:
-	mov %r13, 0x0(%rcx)      
-
-	mov $1, %rax            
-
-end_function:
-	pop %r14                 
-	pop %r13
-	pop %r12
-	pop %rbp
-	pop %rbx
+    cmpq $0, (%rdi)       
+    jne empty
+    movq $1, %rax         
+    ret
+empty:
+	xor %rax,%rax
 	ret
+    
+
--- others/machManager/USAC05/main.c
+++ others/machManager/USAC05/main.c
@@ -1,38 +1,24 @@
 #include <stdio.h>
 #include "asm.h"
 
+#include <stdio.h>
+#include "asm.h"
+
+int buffer[5] = {0};
+int length = 5;  
+int tail = 0;
+int head = 0 ;          
+int value;
 int main() {
+    for (int i = 1; i < 11; i++) {
+        value = i;
+        int res = enqueue_value(buffer, length, &tail, &head, value);
+        printf("Value = %d   || { ", value);
 
-	int length = 5;  // Length of the buffer
-	int buffer[5] = {0};  // Buffer to store the values, initialized to 0
-
-	int head = 0;
-	int tail = 0;
-
-	printf("Initial buffer: ");
-	for (int i = 0; i < length; i++) {
-		printf("%d ", buffer[i]);
-	}
-	printf("\n");
-
-	printf("Inserting values into the buffer:\n");
-
-	for (int i = 1; i <= 7; i++) {  // We try to insert 7 values, more than the buffer length
-		printf("Inserting %d: ", i);
-		int result = enqueue_value(buffer, length, &tail, &head, i);  // Call the assembly function
-
-		printf("Buffer after insertion: ");
-		for (int j = 0; j < length; j++) {
-			printf("%d ", buffer[j]);
-		}
-		printf("\n");
-
-		if (result == 1) {
-			printf("Buffer is full after inserting %d.\n", i);
-		} else {
-			printf("Buffer is not full after inserting %d.\n", i);
-		}
-	}
-
-	return 0;
+        for (int j = 0; j < 5; j++) {
+            printf("%d, ", buffer[j]);
+        }
+        printf("} || res = %d\n", res);
+    }
+    return 0;
 }
